d9867c7a47703fe3ac7700a87e1454ec448cf0e3
David Rodrigues <davidrodrigues@mac-2.home>
2026-05-23 14:33:18 +0200

    fix firebase storage bucket

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAhn5DVTzvEKUBnHaciqNwnxspd6j6jvyc",
   authDomain: "rodappli.firebaseapp.com",
   projectId: "rodappli",
-  storageBucket: "rodappli.appspot.com",
+  storageBucket: "rodappli.firebasestorage.app",
   messagingSenderId: "851960384600",
   appId: "1:851960384600:web:f14f2768eb10919e8f4e2d"
 };
